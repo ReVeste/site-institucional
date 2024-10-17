d7852a8347bd9d1810336dda4825245b6f6e2010
matheus rabello <142695259+matheusrabellosantos@users.noreply.github.com>
2024-10-16 22:26:32 -0300

    integrando a api emailJs ao front

--- reveste/src/pages/Cadastro/Cadastro.jsx
+++ reveste/src/pages/Cadastro/Cadastro.jsx
@@ -4,9 +4,44 @@ import logo from '../../assets/logo.png';
 import reveste from '../../assets/reveste.png';
 import imagemBackground from '../../assets/mulher-trabalhando-sorrindo.png';
 import { useNavigate } from "react-router-dom";
+import emailjs from '@emailjs/browser';
 
 const CadastroForm = () => {
   const navigate = useNavigate();
+
+
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    telefone: '',
+    empresa: '',
+    cnpj: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Substitua com os IDs corretos do EmailJS
+    const serviceID = 'service_lx7aohr'; 
+    const templateID = 'template_l6v390j'; 
+    const publicKey = 'VMQgnW8wc7Ow7oK3C'; 
+
+    // Envia o email usando o emailJS
+    emailjs.send(serviceID, templateID, formData, publicKey)
+      .then((response) => {
+        console.log('Email enviado com sucesso!', response.status, response.text);
+        alert('Email enviado com sucesso!');
+      }, (error) => {
+        console.log('Erro ao enviar o email:', error);
+        alert('Erro ao enviar o email. Por favor, tente novamente.');
+      });
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles['lado-esquerdo']}>
@@ -27,23 +62,23 @@ const CadastroForm = () => {
           <div className={styles['forms-secao']}>
             <div className={styles.agrupador}>
               <label>Nome *</label>
-              <input type="text" />
+              <input type="text" name="nome" value={formData.nome} onChange={handleChange}/>
             </div>
             <div className={styles.agrupador}>
               <label>Email *</label>
-              <input type="email" />
+              <input type="email" name="email" value={formData.email} onChange={handleChange}/>
             </div>
             <div className={styles.agrupador}>
               <label>Telefone *</label>
-              <input type="tel" />
+              <input type="tel" name="telefone" value={formData.telefone} onChange={handleChange}/>
             </div>
             <div className={styles.agrupador}>
               <label>Nome da Empresa *</label>
-              <input type="text" />
+              <input type="text" name="empresa" value={formData.empresa} onChange={handleChange}/>
             </div>
             <div className={styles.agrupador}>
               <label>CNPJ *</label>
-              <input type="text"/>
+              <input type="text" name="cnpj" value={formData.cnpj} onChange={handleChange}/>
             </div>
           </div>
 
@@ -52,7 +87,7 @@ const CadastroForm = () => {
             <label htmlFor="termos">Concordo com os <span className={styles.termosLink}>termos e condições.</span></label>
           </div>
 
-          <button className={styles.enviar}>Enviar</button>
+          <button onClick={handleSubmit} className={styles.enviar}>Enviar</button>
         </div>
       </div>
 
